bc129fc61f25aab0538355ac1a5ce7fc62ac88be
Purna Saud <149823755+Purnasaud@users.noreply.github.com>
2023-11-14 22:42:35 +0545

    Delete Working With Vector.py

--- Working With Vector.py
+++
@@ -1,8 +0,0 @@
-from osgeo import ogr
-import os
-
-DriverName = "ESRI Shapefile"      # e.g.: GeoJSON, ESRI Shapefile
-FileName = 'test.shp'
-driver = ogr.GetDriverByName(DriverName)
-if os.path.exists(FileName):
-     driver.DeleteDataSource(FileName)
